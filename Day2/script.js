8d9fd412091aa2749de6d6c44735b54c1c8edd25
drivelikejehu <pollyfactor@hotmail.com>
2023-01-13 14:20:53 -0500

    array of arrays

--- Day2/script.js
+++ Day2/script.js
@@ -1,4 +1,17 @@
 var fs = require('fs')
-var list = fs.readFileSync('input.txt', 'utf-8')
+var list = fs.readFileSync('input.txt', 'utf-8').replace(/\n/g, ' ').split(' ')
+var arr = []
+var arr2 = []
 
 console.log(list)
+
+for (let i = 0; i < list.length; i++) {
+    arr2.push(list[i])
+    if (i > 0 && i % 2 === 1) {
+        arr.push(arr2)
+        arr2 = []
+    }
+}
+
+
+console.log(arr)
