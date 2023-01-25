8f89a12013b4a7b76f3c8ec32be4f9bae2b9e4c0
drivelikejehu <pollyfactor@hotmail.com>
2023-01-25 15:25:37 -0500

    array of common letters

--- Day3/script.js
+++ Day3/script.js
@@ -1,11 +1,27 @@
 var fs = require('fs')
 var list = fs.readFileSync('input.txt', 'utf-8').split("\n")
 var ruckSacks = []
+var commonLetters = []
 
 for (let i = 0; i < list.length; i++) {
     let arr = []
-    arr.push(list[i])
+    let ruckSack = list[i]
+
+    // separate each rucksack into 2 equal compartments
+    let compartment1 = ruckSack.slice(0, ruckSack.length / 2)
+    let compartment2 = ruckSack.slice(ruckSack.length / 2, ruckSack.length)
+    arr.push(compartment1, compartment2)
     ruckSacks.push(arr)
 }
 
-console.log("RuckSacks: ", ruckSacks)
+// console.log("RuckSacks: ", ruckSacks)
+
+// finding the common letter in each string
+for (let j = 0; j < ruckSacks.length; j++) {
+    let comp1 = new Set(ruckSacks[j][0])
+    let res = ([...new Set(ruckSacks[j][1])]
+        .filter(char => comp1.has(char)))
+        commonLetters.push(res[0])
+}
+
+console.log("common letters: ", commonLetters)
